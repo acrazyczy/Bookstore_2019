da76137f18bae16de58ba077422182bdbe2c598c
acrazyczy <a_crazy_czy@yahoo.com>
2020-01-07 11:57:13 +0800

    fix cin

--- source/main.cpp
+++ source/main.cpp
@@ -9,13 +9,11 @@
 
 int main()
 {
-	std::ifstream in("/mnt/d/Academy/OIACM/Code/Homework/Bookstore_2019/bookstore/test_data/testcase6.txt");
-	std::cin.rdbuf(in.rdbuf());
 	char *cmd = new char [1000];int argc = 0 , qut = 0;char *argv[100];
 	int tot = 0;
 	for (bookstore Douban;!tot || Douban.command(argc , argv);++ tot)
 	{
-		in.getline(cmd , 999 , '\r') , in.get();
+		std::cin.getline(cmd , 999 , '\r') , std::cin.get();
 		int len = strlen(cmd);argc = 0;
 		cmd[len ++] = ' ' , cmd[len] = '\0';
 		bool flag = 1;
